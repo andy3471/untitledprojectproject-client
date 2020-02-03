2f6cdf5d6a4692e4249801202c320f0d5eed974a
Andrew Hargrave <Andrew.Hargrave@oneadvanced.com>
2020-02-03 16:27:53 +0000

    Login Test

---
+++ src/pages/Login.vue
@@ -0,0 +1,32 @@
+<template>
+  <q-page class="flex flex-center">
+    <q-form @submit="logIn" @reset="onReset" class="q-gutter-md">
+      <q-input filled v-model="username" label="Username" hint="Enter your username"></q-input>
+
+      <q-input
+        filled
+        type="password"
+        v-model="password"
+        label="Password"
+        hint="Enter your password"
+      ></q-input>
+
+      <div>
+        <q-btn label="Submit" type="submit" color="primary"></q-btn>
+        <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm"></q-btn>
+      </div>
+    </q-form>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: "PageLogin",
+  data() {
+    return {
+      username: "",
+      password: ""
+    };
+  }
+};
+</script>
