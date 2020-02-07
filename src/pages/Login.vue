4392925fc46ed2352193437526d6b375fe46f56c
Andrew Hargrave <Andrew.Hargrave@oneadvanced.com>
2020-02-07 13:02:39 +0000

    Code Prettier

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,7 +1,12 @@
 <template>
   <q-page class="flex flex-center">
     <q-form @submit="logIn" @reset="onReset" class="q-gutter-md">
-      <q-input filled v-model="username" label="Username" hint="Enter your username"></q-input>
+      <q-input
+        filled
+        v-model="username"
+        label="Username"
+        hint="Enter your username"
+      ></q-input>
 
       <q-input
         filled
@@ -13,7 +18,13 @@
 
       <div>
         <q-btn label="Submit" type="submit" color="primary"></q-btn>
-        <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm"></q-btn>
+        <q-btn
+          label="Reset"
+          type="reset"
+          color="primary"
+          flat
+          class="q-ml-sm"
+        ></q-btn>
       </div>
     </q-form>
   </q-page>
