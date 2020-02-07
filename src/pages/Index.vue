3c75147d96f97d1d51c55b302b3f82cbf3e8a497
Andrew Hargrave <Andrew.Hargrave@oneadvanced.com>
2020-02-07 16:30:11 +0000

    Install Apollo Client

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,14 +1,28 @@
 <template>
-  <q-page class="flex flex-center"></q-page>
+  <q-page class="flex flex-center">
+    <router-link to="/login">Login</router-link>
+  </q-page>
 </template>
 
 <script>
+import gql from "graphql-tag";
+
 export default {
   name: "PageIndex",
   data() {
     return {
-      api: process.env["API_URL"]
+      user: null
     };
+  },
+  apollo: {
+    user: gql`
+      {
+        user(id: 2) {
+          name
+          email
+        }
+      }
+    `
   }
 };
 </script>
