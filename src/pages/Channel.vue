1c36832198cf2320ceb5398c5166881ee134bde6
Andrew Hargrave <Andrew.Hargrave@oneadvanced.com>
2020-03-11 15:32:08 +0000

    LayoutWork

--- src/pages/Channel.vue
+++ src/pages/Channel.vue
@@ -1,19 +1,22 @@
 <template>
-  <q-page padding>
-    <div v-for="m in 100" :key="m">
-      <q-chat-message
-        name="me"
-        avatar="https://cdn.quasar.dev/img/avatar1.jpg"
-        :text="['hey, how are you?']"
-        sent
-      />
-      <q-chat-message
-        name="Jane"
-        avatar="https://cdn.quasar.dev/img/avatar2.jpg"
-        :text="['doing fine, how r you?']"
-      />
-    </div>
-    <div class="absolute-bottom">
+  <q-page>
+    <q-scroll-area style="height: 60vh">
+      <div v-for="m in 100" :key="m">
+        <q-chat-message
+          name="me"
+          avatar="https://cdn.quasar.dev/img/avatar1.jpg"
+          :text="['hey, how are you?']"
+          sent
+        />
+        <q-chat-message
+          name="Jane"
+          avatar="https://cdn.quasar.dev/img/avatar2.jpg"
+          :text="['doing fine, how r you?']"
+        />
+      </div>
+    </q-scroll-area>
+
+    <div class="fixed-bottom message-box">
       <q-editor
         v-model="editor"
         flat
