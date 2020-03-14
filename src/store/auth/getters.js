4f57e4ff05d61212becddc3625398d621bc73d0c
andy3471 <andy@insideindie.net>
2020-03-14 10:21:59 +0000

    Install Capacitor

--- src/store/auth/getters.js
+++ src/store/auth/getters.js
@@ -1,6 +1,13 @@
 export function headers(state) {
     if (state.tokenAuth) {
         const str = 'Bearer ';
-        return { 'Authorization': str.concat(state.token) };
+        return {
+            'Authorization': str.concat(state.token),
+            'Accept': 'application/json'
+        };
+    } else {
+        return {
+            'Accept': 'application/json'
+        }
     }
 }
